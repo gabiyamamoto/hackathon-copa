79442685ec4e82d1cc77ff9c5d9e8fc32998bda7
gabi emi <gabiemi2021@gmail.com>
2026-06-19 08:45:29 -0300

    Sprint 1: A Conexão

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
+  const [ selecoes, setSelecoes ] = useState([]);
+
+  useEffect(() => {
+    async function carregarDados() {
+      try {
+        const response = await fetch(
+          "https://raw.githubusercontent.com/gabiyamamoto/hackathon-copa/main/db.json"
+        );
+
+        const data = await response.json();
+
+        setSelecoes(data);
+
+        console.log("Dados recebidos:");
+        console.log(data);
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    carregarDados();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>DashBoard da Copa</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
